f7999416e6ec4a904c0ed07e200337c39c718448
David Hardy <david.hardy50@gmail.com>
2020-10-01 15:28:02 +0200

    feat:undeployed heroku db

--- server/index.js
+++ server/index.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const router = require('./router');
-const pool = require('./models/pool');
+// const pool = require('./models/pool');
 const cors = require('cors');
 const morgan = require('morgan');
+const dotenv = require('dotenv');
+const Sequelize = require('sequelize')
+dotenv.config();
+// const { Client } = require('pg');
+
+// const client = new Client({
+//   connectionString: 'postgres://hcoyfhcuaawtsf:1b4544fbea3825ad1793f7f5f50f0bede010a11a4963698c057de9d8d9d8c4e3@ec2-54-228-209-117.eu-west-1.compute.amazonaws.com:5432/d8pb16l0oa34hu?ssl=true',
+//   ssl: {
+//     rejectUnauthorized: false
+//   }
+// });
+const sequelize = new Sequelize('postgres://hcoyfhcuaawtsf:1b4544fbea3825ad1793f7f5f50f0bede010a11a4963698c057de9d8d9d8c4e3@ec2-54-228-209-117.eu-west-1.compute.amazonaws.com:5432/d8pb16l0oa34hu?ssl=true', 
+  {
+    protocol: 'postgres',
+    dialect: 'postgres',
+    dialectOptions: {ssl:{rejectUnauthorized: false}},
+  })
 
 const app = express();
-const port = 3020;
+const port = process.env.PORT || 3020;
 
 app.use(morgan('tiny'));
 app.use(express.json());
@@ -14,7 +31,7 @@ app.use(router);
 
 (async () => {
   try {
-    await pool.connect();
+    await sequelize.authenticate();
     console.log('SimpleFi DB connected 🐘');
     app.listen(port, () => {
       console.log(`Solo server listening on localhost:${port} 🎉`)
--- server/models/pool.js
+++ server/models/pool.js
@@ -1,10 +1,10 @@
 const { Pool } = require ('pg');
 
 const pool = new Pool ({
-  user: 'davidhardy',
-  host: 'localhost',
-  database: 'davidhardy',
-  password: 'admin',
+  user: 'hcoyfhcuaawtsf',
+  host: 'ec2-54-228-209-117.eu-west-1.compute.amazonaws.com',
+  database: 'd8pb16l0oa34hu',
+  password: '1b4544fbea3825ad1793f7f5f50f0bede010a11a4963698c057de9d8d9d8c4e3',
   port: 5432
 });
 
