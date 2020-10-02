22c5f17d09cfdbaa0d0b39854f18afb569502773
David Hardy <david.hardy50@gmail.com>
2020-10-02 11:40:20 +0200

    chore: implemenets footer test

---
+++ client/src/components/Tests/Welcome.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {
+  act,
+  render,
+  wait,
+  fireEvent,
+  screen
+} from "@testing-library/react";
+
+import Welcome from "../Welcome/Welcome"
+
+describe("Welcome", () => {
+  it("loads the page content")
+
+  it("routes to the wallet when the connect wallet button is clicked")
+})
