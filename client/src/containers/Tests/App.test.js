572fe8c25788afc9fe9fd4bed278f94c739701b8
David Hardy <david.hardy50@gmail.com>
2020-10-03 13:02:31 +0200

    refactor: app tests to render App beforeall

--- client/src/containers/Tests/App.test.js
+++ client/src/containers/Tests/App.test.js
@@ -9,15 +9,18 @@ import {
 import App from '../App/App';
 import { BrowserRouter } from 'react-router-dom';
 import Apis from '../../apis' 
-jest.mock('ethers')
+jest.mock('ethers') //this is mocking the entire ethers library in the root
 
 describe("App", () => {
 
-  beforeAll(() => {
+  beforeAll( async () => {
     jest.spyOn(Apis, 'getTokens')
-  });
+    jest.spyOn(Apis, 'getFields')
+    // jest.spyOn(Apis, 'getUserFieldTokens')
+    // jest.spyOn(Apis, 'createContracts')
+    // jest.spyOn(Apis, 'getUserBalance')
+    // jest.spyOn(Apis, 'rewinder')
 
-  it("should correctly update states after receiving the database get requests for fields and tokens", async () => {
     await act( () => {
       render(
         <BrowserRouter>
@@ -25,10 +28,21 @@ describe("App", () => {
         </BrowserRouter>
       )
     })
+  });
+
+  it("should run getToken and getFields api calls on render", async () => {
+    expect(Apis.getTokens).toHaveBeenCalledTimes(1);
+    expect(Apis.getFields).toHaveBeenCalledTimes(1);
+    // expect(Apis.getUserFieldTokens).toHaveBeenCalledTimes(1);
+    // expect(Apis.createContracts).toHaveBeenCalledTimes(1);
+    // expect(Apis.getUserBalance).toHaveBeenCalledTimes(1);
+    // expect(Apis.rewinder).toHaveBeenCalledTimes(1);
+  });
+
+  it("should return the correct values from getToken and getFields api calls", async () => {
 
     expect(Apis.getTokens).toHaveBeenCalledTimes(1);
 
   });
-
 
 })
