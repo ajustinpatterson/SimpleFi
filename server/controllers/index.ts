43d9ce7dcfb00ef1c805b027baeadf3be85ba4f1
Justin Patterson <PJ943@yahoo.com>
2020-10-07 15:05:00 +0200

    feat(server): add signup, signin routes
    add middlewares

--- server/controllers/index.ts
+++ server/controllers/index.ts
@@ -1,4 +1,5 @@
+import { signup, signin } from './auth';
 import { getAllTokens, getUserFieldTokens } from './tokens';
 import { getAllFields } from './fields';
 
-export { getAllTokens, getUserFieldTokens, getAllFields };
+export { getAllTokens, getUserFieldTokens, getAllFields, signin, signup };
--- server/router.ts
+++ server/router.ts
@@ -1,11 +1,18 @@
-import { Router } from 'express';
+const {
+  checkDuplicateUsernameOrEmail,
+} = require('./middlewares/verify-signup');
+const verifyToken = require('./middlewares/authJwt');
+import { response, Router } from 'express';
 import * as controllers from './controllers';
 
 const router: Router = Router();
 
 //TODO: create multiple users and record transactions
-router.get('/tokens', controllers.getAllTokens);
-router.get('/fields', controllers.getAllFields);
+router.get('/tokens', verifyToken, controllers.getAllTokens);
+router.get('/fields', verifyToken, controllers.getAllFields);
 router.get('/userfieldtokens/:tokenIds', controllers.getUserFieldTokens);
+
+router.post('/signup', checkDuplicateUsernameOrEmail, controllers.signup);
+router.post('/login', controllers.signin);
 
 export { router };
