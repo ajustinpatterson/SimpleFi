7b417de21465b68938ac68cf08f8e379cd98b37d
Justin Patterson <PJ943@yahoo.com>
2020-10-07 18:59:20 +0200

    fix(server): happy days
    jwtauth.js with right config setting

--- server/middlewares/authJwt.js
+++ server/middlewares/authJwt.js
@@ -5,12 +5,13 @@ const User = db.user;
 
 const verifyToken = (req, res, next) => {
   let token = req.headers['x-access-token'];
+  console.log('token in authjwt is ', token);
   if (!token) {
     return res.status(403).send({
       message: 'No token provided!',
     });
   }
-  jwt.verify(token, config.secret, (err, decoded) => {
+  jwt.verify(token, config.default.secret, (err, decoded) => {
     if (err) {
       return res.status(401).send({
         message: 'Unauthorized!',
